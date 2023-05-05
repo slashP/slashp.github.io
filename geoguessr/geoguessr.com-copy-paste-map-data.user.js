fa41b0ce621f8814f2539757153b9df30dee824a
Per-Kristian Helland <per-kristian.helland@clave.no>
2023-05-05 19:19:35 +0200

    Accept both array and map as input. Added support for regular csv format and pasting lines with either maps links or csv data.

--- geoguessr/geoguessr.com-copy-paste-map-data.user.js
+++ geoguessr/geoguessr.com-copy-paste-map-data.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Copy/paste Geoguessr map data
 // @namespace    slashP
-// @version      2.3.0
+// @version      2.4.0
 // @description  Copy latitude, longitude, heading, pitch and zoom information from Geoguessr maps as JSON data. Add or replace locations in maps by pasting JSON data or Google Maps link(s) in map maker.
 // @author       slashP
 // @match        https://www.geoguessr.com/*
@@ -14,7 +14,7 @@
   const copyMapDataButtonHtml = '<button id="copyMapData" style="margin-left: 1rem;" class="button button--medium button--secondary margin--left-small" type="button"><span class="button__animation"></span><span class="button__label">Copy map data to clipboard</span></button>';
   const downloadMapDataButtonHtml = '<button id="downloadMapData" style="margin-left: 1rem;" class="button button--medium button--secondary margin--left-small" type="button"><span class="button__animation"></span><span class="button__label">Download map data</span></button><span style="margin-left: 10px;" id="copyMapDataFeedback"></span>';
   const importLocationsFromClipboardButtonHtml = '<div class="center-content" id="importLocationsFromClipboardSection"><button id="importLocationsFromClipboard" style="margin-top: 50px;" class="button button--medium button--secondary margin--left-small" type="button"><span class="button__animation"></span><span class="button__label">Try importing locations from clipboard data</span></button></span></div>';
-  const importLocationsFromFileInputHtml = '<div class="center-content" id="importLocationsFromFileSection"><label for="importLocationsFromFile">Try importing locations from file(s)</label><input accept="application/json" multiple type="file" id="importLocationsFromFile" style="margin-top: 50px;" class="button button--medium button--secondary margin--left-small" type="button"><span class="button__animation"></span><span class="button__label"></span></button></span><span style="margin-left: 10px;" id="importLocationsFeedback"></span></div>';
+  const importLocationsFromFileInputHtml = '<div class="center-content" id="importLocationsFromFileSection"><label for="importLocationsFromFile">Try importing locations from file(s)</label><input accept="application/json,text/plain,text/csv" multiple type="file" id="importLocationsFromFile" style="margin-top: 50px;" class="button button--medium button--secondary margin--left-small" type="button"><span class="button__animation"></span><span class="button__label"></span></button></span><span style="margin-left: 10px;" id="importLocationsFeedback"></span></div>';
   const addLocationsButtonHtml = '<div class="center-content" id="addLocationsSection" style="margin-top: 50px; display: none;"><span id="saveExplanation"></span><br /><div style="margin-top: 30px;"><button id="addLocations" class="button button--medium button--danger margin--left-small" type="button"><span class="button__animation"></span><span class="button__label">Add locations and save map</span></button><span style="margin-left: 10px;" id="addLocationsFeedback"></span></div></div>';
   const replaceLocationsButtonHtml = '<div class="center-content" id="replaceLocationsSection" style="margin-top: 50px; display: none;"><span id="replaceLocationsExplanation"></span><br /><div style="margin-top: 30px;"><button id="replaceLocations" class="button button--medium button--danger margin--left-small" type="button"><span class="button__animation"></span><span class="button__label">Replace locations and save map</span></button><span style="margin-left: 10px;" id="replaceLocationsFeedback"></span></div></div>';
   const buttons = [
@@ -26,12 +26,10 @@
     { html: replaceLocationsButtonHtml, containerSelector: ".container__content" },
   ];
   const newMapMakerContainerSelector = "[class*='sidebar_container']";
-  const isNewMapMaker = () => document.querySelector(newMapMakerContainerSelector) !== null;
   const mapId = () => location.href.split('/').pop();
 
   const getExistingMapData = () => {
-    const url = isNewMapMaker() ? `https://www.geoguessr.com/api/v4/user-maps/drafts/${mapId()}` : `https://www.geoguessr.com/api/v3/profiles/maps/${mapId()}`;
-    const coordinatesPropertyName = isNewMapMaker() ? "coordinates" : "customCoordinates";
+    const url = `https://www.geoguessr.com/api/v4/user-maps/drafts/${mapId()}`;
     return fetch(url)
       .then(response => response.json())
       .then(map => ({
@@ -41,7 +39,7 @@
         avatar: map.avatar,
         highlighted: map.highlighted,
         published: map.published,
-        customCoordinates: map[coordinatesPropertyName]
+        customCoordinates: map.coordinates
       }));
   }
   const uniqueBy = (arr, selector) => {
@@ -78,33 +76,55 @@
         });
       });
   }
-  let mapDataFromClipboard = null;
+  let locations = [];
   let existingMap = null;
   const isFullStreetViewUrl = url => url.match(/https:\/\/www\.google\.[a-z.]+\/maps\/@/) !== null;
+
+  const isLineValidCsv = line => {
+    const commaSeparatedValues = line?.split(',') || [];
+    return commaSeparatedValues.length >= 2 && parseFloat(commaSeparatedValues[0]) > -90 && parseFloat(commaSeparatedValues[0]) < 90 && parseFloat(commaSeparatedValues[1]) > -180 && parseFloat(commaSeparatedValues[1]) < 180;
+  }
+
+  const isCsvTextWithLocations = lines => {
+    const firstLine = lines?.split('\n')[0];
+    return isLineValidCsv(firstLine);
+  }
+
   const importLocationsFromClipboard = () => {
     navigator.clipboard.readText().then(text => {
-      if (isFullStreetViewUrl(text)) {
-        importSingleLocationFromLinkInClipboard(text);
+      if (isFullStreetViewUrl(text) || isCsvTextWithLocations(text)) {
+        importLocationsFromLinesInClipboard(text);
       } else {
         importLocations(text);
       }
     });
   }
-  const importSingleLocationFromLinkInClipboard = (linkText) => {
-    const links = linkText.split('\n');
+
+  const csvLocation = l => ({
+    lat: parseFloat(l.split(',')[0]),
+    lng: parseFloat(l.split(',')[1])
+  });
+
+  const getLocationsFromLines = linkText => {
+    const lines = linkText.split('\n');
     const extractNumberFromParameter = (url, param) => parseFloat(url.split(",").slice(2).filter(x => x.indexOf(param) !== -1)[0]);
     let coordinates = [];
-    for (let link of links) {
-      if (!isFullStreetViewUrl(link)) {
+    for (let line of lines) {
+      if (isLineValidCsv(line)) {
+        coordinates.push(csvLocation(line));
         continue;
       }
 
-      const lng = parseFloat(link.split(",")[1]);
-      const lat = parseFloat(link.split(",")[0].split("@")[1]);
-      const heading = extractNumberFromParameter(link, "h");
-      const zoom = (90 - extractNumberFromParameter(link, "y")) / 90 * 2.75; // guesstimated "max" value.
-      const pitch = extractNumberFromParameter(link, "t") - 90;
-      const panoId = link.split("!1s")[1].split("!2e")[0];
+      if (!isFullStreetViewUrl(line)) {
+        continue;
+      }
+
+      const lng = parseFloat(line.split(",")[1]);
+      const lat = parseFloat(line.split(",")[0].split("@")[1]);
+      const heading = extractNumberFromParameter(line, "h");
+      const zoom = (90 - extractNumberFromParameter(line, "y")) / 90 * 2.75; // guesstimated "max" value.
+      const pitch = extractNumberFromParameter(line, "t") - 90;
+      const panoId = line.split("!1s")[1].split("!2e")[0];
       if (!lng || !lat || !heading || !panoId) {
         continue;
       }
@@ -122,25 +142,36 @@
       coordinates.push(coordinate);
     }
 
+    return coordinates;
+  }
+
+  const getLocationsFromCsv = linkText => linkText.split('\n')?.filter(isLineValidCsv).map(csvLocation);
+
+  const importLocationsFromLinesInClipboard = (linkText) => {
+    const coordinates = getLocationsFromLines(linkText);
     const mapText = JSON.stringify({
       customCoordinates: coordinates
     });
     importLocations(mapText);
   }
   const setImportLocationsFeedbackText = text => { document.getElementById("importLocationsFeedback").innerText = text; }
+  const arrayOrCustomCoordinates = obj => Array.isArray(obj) ? obj : obj?.customCoordinates;
 
-  const importLocations = (text, mapAsObject) => {
+  const importLocations = (text, mapOrLocationArray) => {
     try {
       getExistingMapData()
         .then(map => {
-          existingMap = map;
-          mapDataFromClipboard = mapAsObject ? mapAsObject : JSON.parse(text);
-          if (!mapDataFromClipboard?.customCoordinates?.length) {
+          existingMap = {
+            ...map,
+            customCoordinates: map.customCoordinates || []
+          };
+          locations = arrayOrCustomCoordinates(mapOrLocationArray) || arrayOrCustomCoordinates(JSON.parse(text));
+          if (!locations?.length) {
             setImportLocationsFeedbackText("Invalid map data.");
             return;
           }
           const uniqueExistingLocations = uniqueBy(existingMap.customCoordinates, latLngSelector);
-          const uniqueImportedLocations = uniqueBy(mapDataFromClipboard.customCoordinates, latLngSelector);
+          const uniqueImportedLocations = uniqueBy(locations, latLngSelector);
           const uniqueLocations = uniqueBy([...uniqueExistingLocations, ...uniqueImportedLocations], latLngSelector);
           const numberOfLocationsBeingAdded = uniqueLocations.length - uniqueExistingLocations.length;
           const numberOfUniqueLocationsImported = uniqueImportedLocations.length;
@@ -175,7 +206,7 @@
 <span style="color: green;">${numberOfLocationsNotInExistingMap} ${pluralize("addition", numberOfLocationsNotInExistingMap)}</span><br>
 <span style="color: cornflowerblue;">${numberOfLocationEditions} ${pluralize("edition", numberOfLocationEditions)} (different heading/pitch)</span><br>
 <span style="color: coral;">${numberOfExactlyMatchingLocations} exactly same</span>`;
-          document.getElementById("replaceLocationsSection").style.display = numberOfUniqueLocationsImported >= 5 ? "block" : "none";
+          document.getElementById("replaceLocationsSection").style.display = numberOfUniqueLocationsImported >= 0 ? "block" : "none";
 
           document.getElementById("importLocationsFromClipboardSection").style.display = "none";
           document.getElementById("importLocationsFromFileSection").style.display = "none";
@@ -190,12 +221,12 @@
   }
 
   const addLocations = () => {
-    if (!mapDataFromClipboard || !existingMap) {
+    if (!locations?.length || !existingMap) {
       return;
     }
     const setAddLocationsFeedbackText = text => { document.getElementById("addLocationsFeedback").innerText = text; }
     // take unique locations based on lat/lng, keep first entry/original.
-    const uniqueLocations = uniqueBy([...existingMap.customCoordinates, ...mapDataFromClipboard.customCoordinates], latLngSelector);
+    const uniqueLocations = uniqueBy([...existingMap.customCoordinates, ...locations], latLngSelector);
     const newMap = {
       ...existingMap,
       customCoordinates: uniqueLocations
@@ -204,12 +235,12 @@
   }
 
   const replaceLocations = () => {
-    if (!mapDataFromClipboard || !existingMap) {
+    if (!locations?.length || !existingMap) {
       return;
     }
     const setReplaceLocationsFeedbackText = text => { document.getElementById("replaceLocationsFeedback").innerText = text; }
     // take unique locations based on lat/lng, keep first entry/original.
-    const uniqueLocations = uniqueBy(mapDataFromClipboard.customCoordinates, latLngSelector);
+    const uniqueLocations = uniqueBy(locations, latLngSelector);
     const newMap = {
       ...existingMap,
       customCoordinates: uniqueLocations
@@ -218,8 +249,8 @@
   }
 
   function updateMap(newMap, setFeedback) {
-    const url = isNewMapMaker() ? `https://www.geoguessr.com/api/v4/user-maps/drafts/${mapId()}` : `https://www.geoguessr.com/api/v3/profiles/maps/${mapId()}`;
-    const httpMethod =  isNewMapMaker() ? 'PUT' : 'POST';
+    const url = `https://www.geoguessr.com/api/v4/user-maps/drafts/${mapId()}`;
+    const httpMethod = 'PUT';
     fetch(url, {
       method: httpMethod,
       credentials: 'same-origin',
@@ -258,10 +289,17 @@
       let map = {};
       for (let file of this.files) {
         const content = await readFileContent(file);
-        const additionalMap = JSON.parse(content);
-        map = {
-          ...map,
-          customCoordinates: [...map.customCoordinates || [], ...additionalMap.customCoordinates]
+        if (isFullStreetViewUrl(content) || isCsvTextWithLocations(content)) {
+          const coordinates = getLocationsFromLines(content);
+          map = {
+            ...map,
+            customCoordinates: [...map.customCoordinates || [], ...coordinates]
+          }
+        } else {
+          map = {
+            ...map,
+            customCoordinates: [...map.customCoordinates || [], ...arrayOrCustomCoordinates(JSON.parse(content))]
+          }
         }
       }
       importLocations(null, map);
@@ -291,7 +329,7 @@
   document.head.appendChild(style);
   const tryAddButtons = () => {
     setTimeout(() => {
-      if (new RegExp('^https:\/\/www.geoguessr.com\/map-maker/').test(window.location.href) && isNewMapMaker()) {
+      if (new RegExp('^https:\/\/www.geoguessr.com\/map-maker/').test(window.location.href)) {
         const buttonParentElement = document.createElement("div");
         document.querySelector(newMapMakerContainerSelector).appendChild(buttonParentElement);
         buttonParentElement.classList.add("copy-paste-container");
@@ -300,22 +338,6 @@
           const buttonElement = document.createElement("span");
           buttonElement.innerHTML = button.html;
           buttonParentElement.appendChild(buttonElement);
-        }
-        document.getElementById("copyMapData").onclick = copyMapData;
-        document.getElementById("downloadMapData").onclick = downloadMapData;
-        document.getElementById("importLocationsFromClipboard").onclick = importLocationsFromClipboard;
-        document.getElementById("importLocationsFromFile").addEventListener("change", handleFileImportChanged, false);
-        document.getElementById("addLocations").onclick = addLocations;
-        document.getElementById("replaceLocations").onclick = replaceLocations;
-
-        document.addEventListener('paste', e => {
-          importLocationsFromClipboard();
-        });
-      } else if (new RegExp('^https:\/\/www.geoguessr.com\/map-maker/').test(window.location.href)) {
-        for (let button of buttons) {
-          const buttonElement = document.createElement("span");
-          buttonElement.innerHTML = button.html;
-          document.querySelector(button.containerSelector).appendChild(buttonElement);
         }
         document.getElementById("copyMapData").onclick = copyMapData;
         document.getElementById("downloadMapData").onclick = downloadMapData;
